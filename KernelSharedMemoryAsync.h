7e44ee6da4026c1939de7d08e28e1acaef4efa1c
andrejBel <andrej.beliancin3@gmail.com>
2018-03-01 00:16:10 +0100

    playing with cuda mannaged memory and asynchronous operation

---
+++ KernelSharedMemoryAsync.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "Runnable.h"
+#include <vector>
+#include "Filter.h"
+#include <memory>
+#include <string>
+#include "ThreadPool.h"
+
+using namespace std;
+namespace processing
+{
+
+	template<typename T>
+	class KernelSharedMemoryAsync : public Runnable<T>
+	{
+		static_assert(std::is_floating_point<T>::value, "Class KernelSlowConvolution can only be instantiazed with float, double or long double");
+	public:
+
+		KernelSharedMemoryAsync();
+
+		DELETECOPYASSINGMENT(KernelSharedMemoryAsync<T>)
+
+			virtual void run(ImageFactory& image, vector<shared_ptr<AbstractFilter<T>>>& filters, vector<shared_ptr<T>>& results)  override;
+
+		virtual string getDescription() override
+		{
+			return "GPU shared memmory async";
+		}
+
+	private:
+		ThreadPool threadPool_;
+
+		static const int THREADS_NUMBER = 4;
+	};
+
+
+
+
+}
+
+
+
