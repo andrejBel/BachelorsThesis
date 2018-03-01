f1abdd2edbe3d2e5bade84afbd8262e41b7a9066
andrejBel <andrej.beliancin3@gmail.com>
2018-03-02 00:07:21 +0100

    Metaprogramming hell, not faster than unroll

--- KernelSharedMemory.cu
+++ KernelSharedMemory.cu
@@ -19,6 +19,7 @@
 
 #include <thread>
 #include <algorithm>
+#include <type_traits>
 
 using namespace std;
 
@@ -77,15 +78,59 @@ using namespace std;
 	cout << "Block size: " << BLOCK_SIZE << ", TILE_SIZE: " << TILE_SIZE << ", FILTERWIDTH: " << FILTER_WIDTH << ", time: " << m.getTimeMicro() << endl;\
 }
 
+#define IMAD(a, b, c) ( __mul24((a), (b)) + (c) )
+
 namespace processing
 {
+
+	template <typename T, typename U>
+struct X_;
+
+template <typename T, T N>
+struct X_<T, std::integral_constant<T, N>> {};
+
+template <typename T>
+struct X_<T, std::integral_constant<T, 0>> {};
+
+template <typename T, T N>
+struct X : X_<T, std::integral_constant<T, N>> {};
+
+
+	template <typename R, typename T, typename U>
+	struct ForLoop;
+
+	template <typename R ,typename T, T N>
+	struct ForLoop<R,T, std::integral_constant<T, N>>
+	{
+		template< typename F>
+		__device__ __forceinline__ R operator()(F f)
+		{
+			return f(N) + ForLoop<R, T ,N - 1>()(f);
+		}
+
+	};
+
+	template <typename R,typename T, T N>
+	struct ForLoop<T, std::integral_constant<T, -1>>
+	{
+		template<typename F>
+		__device__ __forceinline__ R operator()(F f)
+		{
+			return 0;
+		}
+
+	};
+
+	template <typename R, typename T, T N>
+	struct FORLOOP :public ForLoop<R,T, std::integral_constant<T, N>> {};
+	
 
 	template<typename T, typename int FILTER_WIDTH, typename int BLOCK_SIZE, typename int TILE_SIZE>
 	__global__ void convolutionGPUShared(processing::Filter<T, FILTER_WIDTH> * filter, const int numRows, const int numCols, uchar * inputImage, T * outputImage)
 	{
 		int2 absoluteImagePosition;
-		absoluteImagePosition.x = blockIdx.x * TILE_SIZE + threadIdx.x;
-		absoluteImagePosition.y = blockIdx.y * TILE_SIZE + threadIdx.y;
+		absoluteImagePosition.x = IMAD(blockIdx.x, TILE_SIZE, threadIdx.x);
+		absoluteImagePosition.y = IMAD(blockIdx.y, TILE_SIZE, threadIdx.y);
 		int2 sharedPosition;
 		sharedPosition.x = absoluteImagePosition.x - (FILTER_WIDTH / 2);
 		sharedPosition.y = absoluteImagePosition.y - (FILTER_WIDTH / 2);
@@ -94,24 +139,22 @@ namespace processing
 		int threadY = threadIdx.y;
 		sharedPosition.x = min(max(sharedPosition.x, 0), numCols - 1);
 		sharedPosition.y = min(max(sharedPosition.y, 0), numRows - 1);
-		shared[threadY][threadX] = inputImage[sharedPosition.y * numCols + sharedPosition.x];
+		shared[threadY][threadX] = inputImage[IMAD(sharedPosition.y, numCols, sharedPosition.x)];
 		__syncthreads();
-		const T* filterV = filter->getFilter();
-		T result(0.0);
-		
-
 		if (threadX < TILE_SIZE && threadY < TILE_SIZE && absoluteImagePosition.x < numCols && absoluteImagePosition.y <  numRows)
 		{
-		#pragma unroll FILTER_WIDTH
-			for (int yOffset = 0; yOffset < FILTER_WIDTH; yOffset++)
+			const T* filterV = filter->getFilter();
+			T result = FORLOOP<T,int,FILTER_WIDTH - 1>()([&result, filterV, &shared, threadX, threadY](int yOffset)
 			{
-		#pragma unroll FILTER_WIDTH
-				for (int xOffset = 0; xOffset < FILTER_WIDTH; xOffset++)
+				return FORLOOP<T,int,FILTER_WIDTH - 1>()([&result, filterV, &shared, threadX, threadY, yOffset](int xOffset)
 				{
-					result += filterV[yOffset*FILTER_WIDTH + xOffset] * shared[yOffset + threadY][xOffset + threadX];
-				}
-			}
-			outputImage[absoluteImagePosition.y * numCols + absoluteImagePosition.x] = result;
+					return filterV[IMAD(yOffset, FILTER_WIDTH, xOffset)] * shared[yOffset + threadY][xOffset + threadX];
+				});
+			});
+
+
+
+			outputImage[IMAD(absoluteImagePosition.y, numCols, absoluteImagePosition.x)] = result;
 		}
 	}
 
@@ -159,7 +202,7 @@ namespace processing
 				const dim3 blockSize(BLOCK_SIZE, BLOCK_SIZE);
 				const dim3 gridSize((image.getNumCols() + TILE_SIZE - 1) / TILE_SIZE, (image.getNumRows() + TILE_SIZE - 1) / TILE_SIZE, 1);
 				convolutionGPUShared<T, FILTER_WIDTH, BLOCK_SIZE, TILE_SIZE> << <gridSize, blockSize >> >(ptr, image.getNumRows(), image.getNumCols(), deviceGrayImageIn.get(), deviceGrayImageOut.get());
-				checkCudaErrors(cudaDeviceSynchronize());
+				//checkCudaErrors(cudaDeviceSynchronize());
 				break;
 			}
 			case 5:
@@ -172,7 +215,7 @@ namespace processing
 						const dim3 gridSize((image.getNumCols() + TILE_SIZE - 1) / TILE_SIZE, (image.getNumRows() + TILE_SIZE - 1) / TILE_SIZE, 1);
 						static_assert(BLOCK_SIZE - TILE_SIZE >= (FILTER_WIDTH - 1), "Wrong block and tile size, BLOCKSIZE - TILESIZE >= (FILTERWIDTH - 1)");
 						convolutionGPUShared<T, FILTER_WIDTH, BLOCK_SIZE, TILE_SIZE> << <gridSize, blockSize >> > (ptr, image.getNumRows(), image.getNumCols(), deviceGrayImageIn.get(), deviceGrayImageOut.get());
-						checkCudaErrors(cudaDeviceSynchronize());
+						//checkCudaErrors(cudaDeviceSynchronize());
 				break;
 			}
 			case 7:
@@ -184,7 +227,7 @@ namespace processing
 				const dim3 blockSize(BLOCK_SIZE, BLOCK_SIZE);
 				const dim3 gridSize((image.getNumCols() + TILE_SIZE - 1) / TILE_SIZE, (image.getNumRows() + TILE_SIZE - 1) / TILE_SIZE, 1);
 				convolutionGPUShared<T,FILTER_WIDTH,BLOCK_SIZE, TILE_SIZE><< <gridSize, blockSize >> >(ptr, image.getNumRows(), image.getNumCols(), deviceGrayImageIn.get(), deviceGrayImageOut.get());
-				checkCudaErrors(cudaDeviceSynchronize());
+				//checkCudaErrors(cudaDeviceSynchronize());
 				break;
 			}
 			default:
--- main.cu
+++ main.cu
@@ -10,7 +10,7 @@
 #include "KernelSlowConvolution.cu"
 #include "KernelSlowConvolutionNoEdgeCopy.cu"
 #include "KernelSharedMemory.cu"
-#include "KernelSharedMemoryAsync.cu"
+#include "KernelSharedMemoryManaged.cu"
 #include "Filter.cu"
 #include "CpuSlowConvolution.cpp"
 #include "CPUSlowConvolutionAsync.cpp"
@@ -18,6 +18,7 @@
 #include <algorithm>
 #include "Test.h"
 #include "ThreadPool.h"
+
 using namespace cv;
 using namespace std;
 using namespace processing;
@@ -25,16 +26,58 @@ using namespace processing;
 
 static const string OUTPUT_IMAGE_PATH = "output_img.jpg";
 
+void sleep() 
+{
+	this_thread::sleep_for(chrono::milliseconds(2000));
+}
 
+__global__ void kernel(float *x, int n)
+{
+	int tid = threadIdx.x + blockIdx.x * blockDim.x;
+	for (int i = tid; i < n; i += blockDim.x * gridDim.x) {
+		x[i] = sqrt(pow(3.14159, i));
+	}
+}
+
+const int N =( 1 << 20) * 10;
+
+void launch_kernel()
+{
+	float *data;
+	cudaMalloc(&data, N * sizeof(float));
+
+	kernel << <1, 64 >> >(data, N);
+
+	//cudaStreamSynchronize(0);
+}
 
 
 int main()
 {
 	allocateMemmoryDevice<uchar>(1);
+
+
 	//Test<float>::testAllAgainstCpu();
-	Test<float>::testAloneForManaged(make_shared<KernelSharedMemoryAsync<float>>(), 10);
-	Test<float>::testAloneForManaged(make_shared<KernelSharedMemory<float>>(), 10);
-	Test<float>::testAloneForManaged(make_shared<KernelSlowConvolutionNoEdgeCopy<float>>(), 10);
+	//Test<float>::testAgainstCpuSingleCore(make_shared<KernelSharedMemory<float>>(), 1);
+	//cudaDeviceSetCacheConfig(cudaFuncCache::cudaFuncCachePreferL1);
+	//Test<float>::testAloneForManaged(make_shared<CpuSlowConvolution<float>>(), 2);
+	Test<float>::Builder()
+	.addFilter(Test<float>::get3x3Filter())
+	.addFilter(Test<float>::get3x3Filter())
+	.addFilter(Test<float>::get3x3Filter())
+	.addFilter(Test<float>::get3x3Filter())
+	.addFilter(Test<float>::get3x3Filter())
+	.addFilter(Test<float>::get3x3Filter())
+	.setReplications(10)
+	.addRunnable(make_shared<KernelSharedMemory<float>>()).
+	build()();
+	
+	//Test<float>::testAlone(make_shared<CpuSlowConvolution<float>>(), 2);
+	//Test<float>::testAlone(make_shared<CpuSlowConvolution<float>>(), 2);
+	
+	//Test<float>::testAlone(make_shared<KernelSharedMemory<float>>(), 1);
+	//Test<float>::testAloneForManaged(make_shared<KernelSharedMemory<float>>(), 10);
+	//Test<float>::testAloneForManaged(make_shared<KernelSlowConvolutionNoEdgeCopy<float>>(), 10);
 	//Test<float>::testAloneForManaged(make_shared<KernelSharedMemory<float>>(), 5);
 	//cudaDeviceReset();
 	return 0;
