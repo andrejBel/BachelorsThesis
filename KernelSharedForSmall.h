0a2bb1c2ee11fc389d1a10f80e45c23da28de690
andrejBel <andrej.beliancin3@gmail.com>
2018-03-19 21:36:55 +0100

    Kernel, extremelz poverfull, crops images boundaries, manz posibilities are comented out for trying

---
+++ KernelSharedForSmall.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "Runnable.h"
+#include <vector>
+#include "processing.h"
+#include "Filter.h"
+#include <memory>
+#include <string>
+
+using namespace std;
+namespace processing
+{
+
+	
+	class KernelSharedForSmall : public Runnable
+	{
+		
+	public:
+
+		KernelSharedForSmall();
+
+		DELETECOPYASSINGMENT(KernelSharedForSmall)
+
+		virtual void run(ImageFactory& image, vector<shared_ptr<AbstractFilter>>& filters, vector<shared_ptr<float>>& results)  override;
+
+		virtual string getDescription() override
+		{
+			return "GPU shared memmory for small";
+		}
+
+	private:
+
+	};
+
+
+}
+
+
+
